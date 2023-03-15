05ce4ffd6db3de4c1caf5143062d918cba19c374
MaxPovLv <max.pov.lv@gmail.com>
2023-03-15 19:48:12 +0200

    Add user img

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
+let wrap = document.createElement('div')
+wrap.classList.add('wrap')
+document.body.appendChild(wrap)
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
     .then(value => {
         for (let userItem of value) {
+            let imgDiv = document.createElement('div')
             let divUser = document.createElement('div')
             let userText = document.createElement('h2')
+            imgDiv.innerHTML = `<img class="img" src="/img/pngwing.com%20(1).png" alt="user">`
             userText.innerText = `${userItem.id} ${userItem.name}`
+            imgDiv.classList.add('imgDiv')
             divUser.classList.add('block')
             let wrap = document.getElementsByClassName('wrap')[0]
             wrap.appendChild(divUser)
             let anchor = document.createElement('a')
             anchor.innerText = 'Details'
             anchor.href = `./html/user-details.html?data=${JSON.stringify(userItem)}`
-            divUser.append(userText, anchor)
+            divUser.append(imgDiv, userText, anchor)
         }
     })
